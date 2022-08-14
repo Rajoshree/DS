9cf879856dc53a120cf0af5f57a042dc8c61f1ec
Rajoshree <rajoshree911@gmail.com>
2022-08-14 13:59:13 +0530

    initial commit

---
+++ Linkedlist/src/com/datastructure/exception/ReachedLastNodeException.java
@@ -0,0 +1,24 @@
+package com.datastructure.exception;
+
+public class ReachedLastNodeException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ReachedLastNodeException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+	public ReachedLastNodeException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
